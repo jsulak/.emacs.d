83de6544079c04de12ff1f219e915af2dfe8e491
James Sulak <jsulak@gmail.com>
2009-10-12 20:50:59 -0500

    Added custom.el

---
+++ custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(bm-highlight-style (quote bm-highlight-only-fringe))
+ '(column-number-mode t)
+ '(ecb-gzip-setup (quote cons))
+ '(ecb-layout-window-sizes (quote (("left8" (0.23863636363636365 . 0.2903225806451613) (0.23863636363636365 . 0.24193548387096775) (0.23863636363636365 . 0.2903225806451613) (0.23863636363636365 . 0.16129032258064516)))))
+ '(ecb-mouse-click-destination (quote left-top))
+ '(ecb-options-version "2.32")
+ '(ecb-tar-setup (quote cons))
+ '(ecb-wget-setup (quote cons))
+ '(erc-nick "luddite")
+ '(erc-user-full-name "luddite")
+ '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "_sgbak")))
+ '(js2-basic-offset 3)
+ '(js2-highlight-level 3)
+ '(js2-indent-on-enter-key t)
+ '(nxml-auto-insert-xml-declaration-flag t)
+ '(nxml-slash-auto-complete-flag t)
+ '(show-paren-mode t)
+ '(tabbar-mode t)
+ '(tabbar-separator (quote (" | ")))
+ '(tool-bar-mode nil)
+ '(visual-line-fringe-indicators (quote (nil right-curly-arrow))))
