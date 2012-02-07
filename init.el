585adce065c1fa5c143a61c7f515e5a57bd5480f
James Sulak <jsulak@gmail.com>
2012-02-07 08:43:01 -0600

    Yaml-mode now fetched by Marmalade.

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(bm csharp-mode cygwin-mount find-file-in-project ruby-end smex undo-tree yasnippet-bundle)
+(defvar my-packages '(bm csharp-mode cygwin-mount find-file-in-project ruby-end smex undo-tree yaml-mode yasnippet-bundle)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
