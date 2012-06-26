a0e13fe352378fc6306a50c003a34eb3b652bfb9
James Sulak <jsulak@gmail.com>
2012-06-25 21:08:20 -0500

    Now using local forked jshint-mode.

--- james-osx.el
+++ james-osx.el
@@ -111,7 +111,8 @@ environment."
 (require 'tramp)
 
 ;; JS hint mode
-(add-to-list  'load-path "/usr/local/lib/node_modules/jshint-mode")
+;; (add-to-list  'load-path "/usr/local/lib/node_modules/jshint-mode")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/external/jshint-mode"))
 (require 'flymake-jshint)
 (add-hook 'js-mode-hook
          (lambda () (flymake-mode t)))
