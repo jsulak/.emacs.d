f165975a89c468f9de515d95d656d920c5cab228
James Sulak <jsulak@gmail.com>
2013-04-07 19:59:32 -0500

    Make programming notebook an actual minor mode.

--- programming_notebook/programming-notebook.el
+++ programming_notebook/programming-notebook.el
@@ -1,12 +1,35 @@
 
-(defvar pn-notebook-dir "~/Dropbox/programming_notebook/")
+(defvar pn-mode-keymap (make-keymap) "Programming notebook keymap.")
+(define-key pn-mode-keymap (kbd "C-<f10>") 'pn-insert-chrome-link)
+
+(defgroup programming-notebook nil
+  "Programming notebook"
+  :group 'text
+  :prefix "pn-")
+
+(defcustom pn-notebook-dir "~/Dropbox/programming_notebook/"
+  "Directory containing programming notebook"
+  :type 'string
+  :group 'programming-notebook)
+  
+
+(define-minor-mode programming-notebook-mode
+  "Toggle Programming notebook mode."  
+  :init-value nil
+  ;; Mode line indicator
+  :lighter " Prog-Notebook"
+  :keymap pn-mode-keymap
+  :group 'programming-notebook)
+
 
 (defun pn-open-todays-entry ()
   (interactive)
   (find-file
    (format
     (concat (file-name-as-directory pn-notebook-dir) "%s.md")
-    (format-time-string "%Y%m%d"))))
+    (format-time-string "%Y%m%d")))
+  (programming-notebook-mode)
+  )
 
 (defun pn-insert-chrome-link ()
   (interactive)
