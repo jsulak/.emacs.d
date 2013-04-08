572a0235c6a7861faff6a7f6bd44413fba635215
James Sulak <jsulak@gmail.com>
2013-04-07 20:11:32 -0500

    Date format as custom var (programming-notebook mode).

--- programming_notebook/programming-notebook.el
+++ programming_notebook/programming-notebook.el
@@ -11,7 +11,11 @@
   "Directory containing programming notebook"
   :type 'string
   :group 'programming-notebook)
-  
+
+(defcustom pn-date-format "%Y%m%d"
+  "Date format of daily entries in programming notebook"
+  :type 'string
+  :group 'programming-notebook)
 
 (define-minor-mode programming-notebook-mode
   "Toggle Programming notebook mode."  
@@ -28,7 +32,7 @@
   (find-file
    (format
     (concat (file-name-as-directory pn-notebook-dir) "%s.md")
-    (format-time-string "%Y%m%d")))
+    (format-time-string pn-date-format)))
   (programming-notebook-mode)
   )
 
