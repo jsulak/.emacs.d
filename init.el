948916b056f3af8b54f5028bef8f2225567ce426
James Sulak <jsulak@gmail.com>
2014-11-22 21:00:20 -0600

    Whitespace.

--- init.el
+++ init.el
@@ -26,20 +26,20 @@
 (package-initialize)
 
 (unless (and (file-exists-p "~/.emacs.d/elpa/archives/marmalade")
-             (file-exists-p "~/.emacs.d/elpa/archives/gnu")
-             (file-exists-p "~/.emacs.d/elpa/archives/melpa")
-	     (file-exists-p "~/.emacs.d/elpa/archives/melpa-stable"))
+			 (file-exists-p "~/.emacs.d/elpa/archives/gnu")
+			 (file-exists-p "~/.emacs.d/elpa/archives/melpa")
+		 (file-exists-p "~/.emacs.d/elpa/archives/melpa-stable"))
   (package-refresh-contents))
 
 (defun packages-install (&rest packages)
   (mapc (lambda (package)
-          (let ((name (car package))
-                (repo (cdr package)))
-            (when (not (package-installed-p name))
-              (let ((package-archives (list repo)))
-                (package-initialize)
-                (package-install name)))))
-        packages)
+		  (let ((name (car package))
+				(repo (cdr package)))
+			(when (not (package-installed-p name))
+			  (let ((package-archives (list repo)))
+				(package-initialize)
+				(package-install name)))))
+		packages)
   (package-initialize)
   (delete-other-windows))
 
@@ -63,7 +63,7 @@
    (cons 'powerline melpa-stable)
    (cons 'ruby-end melpa)
    (cons 'smartparens melpa)
-   (cons 'smex melpa)   
+   (cons 'smex melpa)
    (cons 'soft-morning-theme melpa)
    (cons 'tangotango-theme melpa)
    (cons 'undo-tree melpa)
@@ -71,7 +71,7 @@
    (cons 'yaml-mode melpa)))
 
 (condition-case nil
-    (init--install-packages)
+	(init--install-packages)
   (error
    (package-refresh-contents)
    (init--install-packages)))
@@ -93,11 +93,11 @@
 
 (cond ((or (eq system-type 'gnu/linux)
 	  (eq system-type 'linux))
-       (load-file "~/.emacs.d/site-lisp/james-linux.el"))
-      ((eq system-type 'darwin)
-       (load-file "~/.emacs.d/site-lisp/james-osx.el"))
-      ((eq system-type 'windows-nt)
-       (load-file "~/.emacs.d/site-lisp/james-windows.el")))
+	   (load-file "~/.emacs.d/site-lisp/james-linux.el"))
+	  ((eq system-type 'darwin)
+	   (load-file "~/.emacs.d/site-lisp/james-osx.el"))
+	  ((eq system-type 'windows-nt)
+	   (load-file "~/.emacs.d/site-lisp/james-windows.el")))
 
 ;; Local initialization options can be saved in local.el
 (setq local-init (concat user-emacs-directory "local.el"))
@@ -146,27 +146,27 @@
 (setq eshell-aliases-file (concat user-emacs-directory "eshell-aliases"))
 
 (setq eshell-save-history-on-exit t
-      eshell-buffer-shorthand t
-      eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'")
+	  eshell-buffer-shorthand t
+	  eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'")
 
 ;;;###autoload
 (eval-after-load 'esh-opt
   '(progn
-     (require 'em-prompt)
-     (require 'em-term)
-     (require 'em-cmpl)
-     (setenv "PAGER" "cat")
-     (add-hook 'eshell-mode-hook ;; for some reason this needs to be a hook
-               '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-bol)))     
-     (setq eshell-cmpl-cycle-completions nil)
+	 (require 'em-prompt)
+	 (require 'em-term)
+	 (require 'em-cmpl)
+	 (setenv "PAGER" "cat")
+	 (add-hook 'eshell-mode-hook ;; for some reason this needs to be a hook
+			   '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-bol)))
+	 (setq eshell-cmpl-cycle-completions nil)
 
-     ;; TODO: submit these via M-x report-emacs-bug
-     (add-to-list 'eshell-visual-commands "ssh")
-     (add-to-list 'eshell-visual-commands "tail")
-     (add-to-list 'eshell-command-completions-alist
-                  '("gunzip" "gz\\'"))
-     (add-to-list 'eshell-command-completions-alist
-                  '("tar" "\\(\\.tar|\\.tgz\\|\\.tar\\.gz\\)\\'"))))
+	 ;; TODO: submit these via M-x report-emacs-bug
+	 (add-to-list 'eshell-visual-commands "ssh")
+	 (add-to-list 'eshell-visual-commands "tail")
+	 (add-to-list 'eshell-command-completions-alist
+				  '("gunzip" "gz\\'"))
+	 (add-to-list 'eshell-command-completions-alist
+				  '("tar" "\\(\\.tar|\\.tgz\\|\\.tar\\.gz\\)\\'"))))
 
 
 ;; ================================
@@ -174,8 +174,8 @@
 ;; ================================
 
 (add-hook 'js-mode-hook (lambda ()
-                          (require 'js-mode-expansions)
-                          (er/add-js-mode-expansions)))
+						  (require 'js-mode-expansions)
+						  (er/add-js-mode-expansions)))
 (setq pending-delete-mode t)
 
 ;; Multiple cursors
@@ -201,16 +201,16 @@
 (electric-layout-mode t)
 
 ;;  electric layout doesn't work right with js-mode
-(defun james-js-mode-hook ()  
+(defun james-js-mode-hook ()
   (electric-layout-mode -1))
 (add-hook 'js-mode-hook 'james-js-mode-hook)
 
 ;; Use lambda for anonymous functions
 ;; (font-lock-add-keywords
  ;; 'js-mode `(("\\(function\\) *("
-              ;; (0 (progn (compose-region (match-beginning 1)
-                                        ;; (match-end 1) "\u0192")
-                        ;; nil)))))
+			  ;; (0 (progn (compose-region (match-beginning 1)
+										;; (match-end 1) "\u0192")
+						;; nil)))))
 
 ;; Use right arrow for return in one-line functions
 ;; (font-lock-add-keywords
@@ -222,8 +222,8 @@
 ;; Add more file types to find-file-in-project
 (defvar ffip-patterns
   '("*.html" "*.org" "*.txt" "*.md" "*.el" "*.clj" "*.py" "*.rb" "*.js" "*.pl"
-    "*.sh" "*.erl" "*.hs" "*.ml" "*.py" "*.xslt" "*.xsl" "*.xpl" "*.cs" "*.zsh"
-    "*.erb" "*.coffee" "*.xml" "*.acl" "*.bat" "*.cmd" "*.xqy" "*.xqm")
+	"*.sh" "*.erl" "*.hs" "*.ml" "*.py" "*.xslt" "*.xsl" "*.xpl" "*.cs" "*.zsh"
+	"*.erb" "*.coffee" "*.xml" "*.acl" "*.bat" "*.cmd" "*.xqy" "*.xqm")
   "List of patterns to look for with `find-file-in-project'.")
 
 (setq ns-pop-up-frames nil)
@@ -248,13 +248,13 @@
 ;; (flx-ido-mode 1)
 ;(setq ido-use-faces nil)
 (setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-auto-merge-work-directories-length nil
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-use-virtual-buffers t
-      ido-handle-duplicate-virtual-buffers 2
-      ido-max-prospects 15)
+	  ido-enable-flex-matching t
+	  ido-auto-merge-work-directories-length nil
+	  ido-create-new-buffer 'always
+	  ido-use-filename-at-point 'guess
+	  ido-use-virtual-buffers t
+	  ido-handle-duplicate-virtual-buffers 2
+	  ido-max-prospects 15)
 
 ;;paren highlighting
 (require 'smartparens-config)
@@ -297,19 +297,19 @@
   "When called interactively with no active region, copy a single line instead."
   (interactive
    (if mark-active (list (region-beginning) (region-end))
-     (message "Copied line")
-     (list (line-beginning-position)
+	 (message "Copied line")
+	 (list (line-beginning-position)
 	   (line-end-position)))))
 (defadvice kill-region (before slick-cut activate compile)
   "When called interactively with no active region, kill a single line instead."
   (interactive
    (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
+	 (list (line-beginning-position)
 	   (line-end-position)))))
 
 ;; Activate winner mode
 (when (fboundp 'winner-mode)
-      (winner-mode 1))
+	  (winner-mode 1))
 
 ;;Prevent backup files from being made
 (setq make-backup-files nil)
@@ -330,10 +330,10 @@
 ;; From http://emacsblog.org/page/5/
 (define-key isearch-mode-map (kbd "C-o")
   (lambda ()
-    (interactive)
-    (let ((case-fold-search isearch-case-fold-search))
-      (occur (if isearch-regexp isearch-string
-               (regexp-quote isearch-string))))))
+	(interactive)
+	(let ((case-fold-search isearch-case-fold-search))
+	  (occur (if isearch-regexp isearch-string
+			   (regexp-quote isearch-string))))))
 
 
 ;; Enable camel-case awareness in all programming modes
@@ -341,25 +341,25 @@
 (add-hook 'prog-mode-hook 'subword-mode)
 
 ;; ========================
-;; Major modes 
+;; Major modes
 ;; ========================
 
 (require 'mmm-auto)
 
 (mmm-add-classes
  '((html-rvt
-    :submode tcl-mode
-    :delimiter-mode nil
-    :front "<\\?[=]?"
-    :front-offset 1
-    :back-offset 1
-    :back "\\?>")))
+	:submode tcl-mode
+	:delimiter-mode nil
+	:front "<\\?[=]?"
+	:front-offset 1
+	:back-offset 1
+	:back "\\?>")))
 
 (setq mmm-submode-decoration-level 0)
 (setq mmm-global-mode 'maybe)
 (mmm-add-mode-ext-class 'html-mode "\\.rvt\\'" 'html-rvt)
 (setq auto-mode-alist (append (list (cons "\\.rvt\\'" 'html-mode))
-                    auto-mode-alist))
+					auto-mode-alist))
 
 
 (setq flymake-run-in-place nil)
@@ -379,8 +379,8 @@
 ;; nxml mode
 (setq rng-schema-locating-files (quote ("schemas.xml" "~/.emacs.d/nxml-mode/schema/schemas.xml")))
 (setq auto-mode-alist
-        (cons '("\\.\\(xml\\|xsl\\|xslt\\|rng\\|xhtml\\|xpr\\|xspec\\|xpl\\)\\'" . nxml-mode)
-	      auto-mode-alist))
+		(cons '("\\.\\(xml\\|xsl\\|xslt\\|rng\\|xhtml\\|xpr\\|xspec\\|xpl\\)\\'" . nxml-mode)
+		  auto-mode-alist))
 
 ;; yaml
 (require 'yaml-mode)
@@ -389,30 +389,30 @@
 ;;Add support for dos batch files
 (require 'dosbat)
 (setq auto-mode-alist
-        (cons '("\\.\\(bat\\|cmd\\)\\'" . bat-mode)
-	      auto-mode-alist))
+		(cons '("\\.\\(bat\\|cmd\\)\\'" . bat-mode)
+		  auto-mode-alist))
 
 
 ;; Add support for scss to css mode
 (setq auto-mode-alist
-      (cons '("\\.\\(scss\\)\\'" . css-mode)
-            auto-mode-alist))
+	  (cons '("\\.\\(scss\\)\\'" . css-mode)
+			auto-mode-alist))
 ;; Automatically load rainbow mode in css mode
 (add-hook 'css-mode-hook 'rainbow-mode)
 
-;;Add ACL mode mode 
+;;Add ACL mode mode
 (autoload 'acl-mode "acl-mode")
 (setq auto-mode-alist (append (list (cons "\\.acl\\'" 'acl-mode))
-                               auto-mode-alist))
+							   auto-mode-alist))
 
 ;; Add markdown mode
 (setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
+	  (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
 ;; This removes unsightly ^M characters that would otherwise
 ;; appear in the output of java applications.
 (add-hook 'comint-output-filter-functions
-          'comint-strip-ctrl-m)
+		  'comint-strip-ctrl-m)
 
 
 ;; =========================
@@ -433,7 +433,7 @@
 
 ;; This turns off filename completion everywhere because it crashes in js.
 ;; It would be better to do it for js2-mode only
-(defun ac-common-setup ())  
+(defun ac-common-setup ())
 (ac-config-default)
 
 (ac-set-trigger-key "TAB")
@@ -441,8 +441,8 @@
 (require 'etags)
 (setq tags-revert-without-query 1)
 
-(add-to-list 'load-path 
-             "~/.emacs.d/external/yasnippet")
+(add-to-list 'load-path
+			 "~/.emacs.d/external/yasnippet")
 (require 'yasnippet)
 ;; (yas/initialize) ::)
 (yas/global-mode 1)
@@ -458,25 +458,25 @@
 
 ;; make bookmarks persistent as default
 (setq-default bm-buffer-persistence t)
- 
+
 ;; Loading the repository from file when on start up.
 (add-hook' after-init-hook 'bm-repository-load)
- 
+
 ;; Restoring bookmarks when on file find.
 (add-hook 'find-file-hooks 'bm-buffer-restore)
- 
+
 ;; Saving bookmark data on killing a buffer
 (add-hook 'kill-buffer-hook 'bm-buffer-save)
- 
+
 ;; Saving the repository to file when on exit.
 ;; kill-buffer-hook is not called when emacs is killed, so we
 ;; must save all bookmarks first.
 (add-hook 'kill-emacs-hook '(lambda nil
-                              (bm-buffer-save-all)
-                              (bm-repository-save)))
+							  (bm-buffer-save-all)
+							  (bm-repository-save)))
 
 ;; Bookmark customizations
-(setq 
+(setq
   bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
   bookmark-save-flag 1)                        ;; autosave each change)
 
@@ -491,8 +491,8 @@
 (require 'server)
 (when (equal window-system 'w32)
   (defun server-ensure-safe-dir (dir) "Noop" t)) ; Suppress error "directory
-                                                 ; ~/.emacs.d/server is unsafe"
-                                                 ; on windows.
+												 ; ~/.emacs.d/server is unsafe"
+												 ; on windows.
 (server-start)
 
 ;; ======================
@@ -523,4 +523,3 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key "\C-x\C-m" 'smex)
 (global-set-key (kbd "C-x m") 'smex)
-
