f930c68b900302f3253375d174909ed9ff5d86a9
James Sulak <jsulak@gmail.com>
2018-10-20 10:43:30 -0500

    Removed autocomplete mode.
    I don't use it, and it seems like company mode is the future

--- init.el
+++ init.el
@@ -308,29 +308,6 @@
 		  'comint-strip-ctrl-m)
 
 
-;; =========================
-;; External packages
-;; =========================
-
-;; Auto-complete
-(add-to-list 'load-path "~/.emacs.d/external/autocomplete/")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/external/autocomplete/ac-dict")
-(setq ac-auto-start 2)
-(setq ac-ignore-case nil)
-(global-auto-complete-mode t)
-(add-to-list 'ac-modes 'html-mode)
-
-;; Attempt to fix css autocomplete silliness
-(define-key ac-complete-mode-map "\r" nil)
-
-;; This turns off filename completion everywhere because it crashes in js.
-;; It would be better to do it for js2-mode only
-(defun ac-common-setup ())
-(ac-config-default)
-
-(ac-set-trigger-key "TAB")
-
 ;; =======================
 ;; Server
 ;; =======================
@@ -346,7 +323,6 @@
 ;; Diminish (clean up) mode line
 ;; ======================
 
-(diminish 'auto-complete-mode)
 (diminish 'smartparens-mode)
 (diminish 'undo-tree-mode)
 ;; (diminish 'yas-minor-mode)
