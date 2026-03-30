0229d78611568ea8d40f8a8703164448035b369b
James Sulak <jsulak@gmail.com>
2026-03-29 19:13:00 -0500

    Updated frame title

--- site-lisp/james-gui.el
+++ site-lisp/james-gui.el
@@ -5,12 +5,15 @@
 ;; Make default text a bit muted
 (setq modus-vivendi-palette-overrides
       '((fg-main "#d0d0d0")))
+
 (load-theme 'modus-vivendi t)
 
 (set-face-attribute 'default nil :font "JetBrains Mono-14")
 
 ;; window frame title
-(setq frame-title-format "Emacs %f")
+(setq frame-title-format
+      '(:eval (let ((path (or buffer-file-name default-directory "")))
+                (concat "Emacs " (abbreviate-file-name path)))))
 
 (setq frame-resize-pixelwise t)
 (setq window-resize-pixelwise t)
